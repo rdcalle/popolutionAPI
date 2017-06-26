0a8ebb9aba5ed03a679044dcc3ade65a93b1e579
Alejandro Armenteros Rodelgo <aarmenterosr@gmail.com>
2017-06-26 21:59:38 +0200

    Added middleware

--- src/middleware/debug.js
+++ src/middleware/debug.js
@@ -1,5 +1,4 @@
 exports.printReq = (req, res, next) => {  
-  debugger
   console.info(req.url)
   console.info(req.body)
   console.info(req.headers)
--- src/routes/index.js
+++ src/routes/index.js
@@ -3,19 +3,20 @@ const router = express.Router();
 
 const auth = require('../auth');  
 const middleware = require('../middleware/auth');
+const debug = require('../middleware/debug');
 
 const config = require('../config');
 const jwtService = require('../services/jwt');
 
 
-router.post('/auth/signup', auth.emailSignup);  
-router.post('/auth/login', auth.emailLogin);
+router.post('/auth/signup', debug.printReq, auth.emailSignup);  
+router.post('/auth/login', debug.printReq,  auth.emailLogin);
 
 router.get('/prueba', (req, res) => res.status(200).send({data: "mensaje de prueba"}));
 router.get('/auth/token', auth.checkToken);
 
 router.get('/private', 
-						middleware.ensureAuthenticated, 
+						middleware.ensureAuthenticated,
 						(req, res) => res.status(200).send("Tu token esta bien " + req.user.email));
 
 
